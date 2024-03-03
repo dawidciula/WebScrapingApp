8c93acb834c81bda2994ab78baefe8d65660e47a
dawidciula <84280914+dawidciula@users.noreply.github.com>
2024-03-03 17:42:38 +0100

    scraping added

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    url = "https://www.skapiec.pl"
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    return render_template('index.html', content=soup.prettify())
 
 
 if __name__ == '__main__':
